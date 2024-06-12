f889b675d22550ea687d908d93c78f84a07e4898
nhannn87dn <nhannn87dn@gmail.com>
2024-06-12 19:38:21 +0700

    'deplpoy'

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+
+  return (
+    <div className='container'>
+      <h1 className='text-3xl font-bold underline'>Hello Shadcn ui</h1>
+      <Button>Pimary Button</Button>
+    </div>
+  )
+}
+
+export default App
